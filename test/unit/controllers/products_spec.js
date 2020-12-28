e3cdd1ccecf578413339dceec504607f3bcd575e
Beatriz <beatrizsallesss@gmail.com>
2020-12-28 16:54:00 -0300

    ✅ test: Created unit test.

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,10 @@
+import ProductsController from '../../../src/controllers/products';
+
+describe('Controllers: Products', () => {
+  describe('get() products', () =>{
+    it('Should return a list of products', () => {
+      
+      const productsController = new ProductsController();
+    });
+  });
+});
