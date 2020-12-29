c8f06a47bf444c0783b6e524fb1309e840bfdf06
Beatriz <beatrizsallesss@gmail.com>
2020-12-29 14:39:00 -0300

    ✅ test: Changing initialization.

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,15 +1,23 @@
-describe('Routes: Products', () => {
+describe("Routes: Products", () => {
+  let request;
+  let app;
+
+  before(async () => {
+    app = await setupApp();
+    request = supertest(app);
+  });
+
+  after(async () => await app.database.connection.close());
+
   const defaultProduct = {
-    name: 'Default product',
-    description: 'product description',
+    name: "Default product",
+    description: "product description",
     price: 100
   };
 
-  describe('GET /products', () => {
-    it('Should return a list of products', done => {
-
-      request
-      .get('/products').end((err, res) => {
+  describe("GET /products", () => {
+    it("should return a list of products", done => {
+      request.get("/products").end((err, res) => {
         expect(res.body[0]).to.eql(defaultProduct);
         done(err);
       });
