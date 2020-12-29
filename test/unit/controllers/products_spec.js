5868a21c16831d6dfbe6bf5fe1a166252f7aaaf8
Beatriz <beatrizsallesss@gmail.com>
2020-12-29 15:45:03 -0300

    ✅ test: Created error 400 in test.

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,25 +1,49 @@
 import ProductsController from '../../../src/controllers/products';
 import sinon from 'sinon';
+import Product from '../../../src/models/product';
 
-describe('Routes: Products', () => {
-  const defaultProduct = [{
-    name: 'Default product',
-    description: 'product description',
-    price: 100
-  }];
+describe('Controllers: Products', () => {
+  const defaultProduct = [
+    {
+      name: 'Default product',
+      description: 'product description',
+      price: 100
+    }
+  ];
 
-  describe('get() products', () =>{
-    it('Should return a list of products', () => {
-      const request ={};
+  describe('get() products', () => {
+    it('Should return a list of products', async () => {
+      const request = {};
       const response = {
         send: sinon.spy()
       };
 
-      const productsController = new ProductsController();
-      productsController.get(request, response);
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).resolves(defaultProduct);
 
-      expect(response.send.called).to.be.true;
-      expect(response.send.calledWith(defaultProduct)).to.be.true;
+      const productsController = new ProductsController(Product);
+
+      await productsController.get(request, response);
+
+      sinon.assert.calledWith(response.send, defaultProduct);
+    });
+
+    it('Should return 400 when an error occurs', async () => {
+      const request = {};
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      };
+
+      response.status.withArgs(400).returns(response);
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).rejects({ message: 'Error' });
+
+      const productsController = new ProductsController(Product);
+
+      await productsController.get(request, response);
+
+      sinon.assert.calledWith(response.send, 'Error');
     });
   });
 });
