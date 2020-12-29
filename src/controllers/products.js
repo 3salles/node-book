a852bc08078f4041dcbd19cf4a13f5732127cb2c
Beatriz <beatrizsallesss@gmail.com>
2020-12-29 15:44:29 -0300

    ✅ test: Updated controller to use model.

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,15 @@
 class ProductsController {
+  constructor(Product) {
+    this.Product = Product;
+  }
 
-  get(req, res) {
-    return res.send([{
-      name: 'Default product',
-      description: 'product description',
-      price: 100
-    }]);
+  async get(req, res) {
+    try {
+      const products = await this.Product.find({});
+      res.send(products);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
   }
 }
 
