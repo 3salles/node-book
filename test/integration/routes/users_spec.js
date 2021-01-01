0dda32e5c0c2426ac2ca132a69e4e73d47603e49
Beatriz <beatrizsallesss@gmail.com>
2020-12-31 21:58:38 -0300

    ✅ test: Created user integration test.

---
+++ test/integration/routes/users_spec.js
@@ -0,0 +1,103 @@
+import User from '../../../src/models/user';
+
+describe('Routes: Users', () => {
+  const defaultId = '56cb91bdc3464f14678934ca';
+  const defaultAdmin = {
+    name: 'Jhon Doe',
+    email: 'jhon@mail.com',
+    password: '123password',
+    role: 'admin'
+  };
+  const expectedAdminUser = {
+    _id: defaultId,
+    name: 'Jhon Doe',
+    email: 'jhon@mail.com',
+    role: 'admin'
+  };
+
+  beforeEach(async () => {
+    const user = new User(defaultAdmin);
+    user._id = '56cb91bdc3464f14678934ca';
+    await User.deleteMany({});
+    await user.save();
+  });
+
+  afterEach(async () => await User.deleteMany({}));
+
+  describe('GET /users', () => {
+    it('Should return a list of users', done => {
+      request.get('/users').end((err, res) => {
+        expect(res.body).to.eql([expectedAdminUser]);
+        done(err);
+      });
+    });
+
+    context('When an id is specified', done => {
+      it('Should return 200 with one user', done => {
+        request.get(`/users/${defaultId}`).end((err, res) => {
+          expect(res.statusCode).to.eql(200);
+          expect(res.body).to.eql([expectedAdminUser]);
+          done(err);
+        });
+      });
+    });
+  });
+
+  describe('POST /users', () => {
+    context('When posting an user', () => {
+      it('Should return a new user with status code 201', done => {
+        const customId = '56cb91bdc3464f14678934ba';
+        const newUser = Object.assign(
+          {},
+          { _id: customId, __v: 0 },
+          defaultAdmin
+        );
+        const expectedSavedUser = {
+          _id: customId,
+          name: 'Jhon Doe',
+          email: 'jhon@mail.com',
+          role: 'admin'
+        };
+
+        request
+          .post('/users')
+          .send(newUser)
+          .end((err, res) => {
+            expect(res.statusCode).to.eql(201);
+            expect(res.body).to.eql(expectedSavedUser);
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('PUT /users/:id', () => {
+    context('When editing an user', () => {
+      it('Should update the user and return 200 as status code', done => {
+        const customUser = {
+          name: 'Din Doe'
+        };
+        const updatedUser = Object.assign({}, defaultAdmin, customUser);
+
+        request
+          .put(`/users/${defaultId}`)
+          .send(updatedUser)
+          .end((err, res) => {
+            expect(res.status).to.eql(200);
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('DELETE /users/:id', () => {
+    context('When deleting an user', () => {
+      it('Should delete an user and return 204 as status code', done => {
+        request.delete(`/users/${defaultId}`).end((err, res) => {
+          expect(res.status).to.eql(204);
+          done(err);
+        });
+      });
+    });
+  });
+});
