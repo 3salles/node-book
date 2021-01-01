532ba808d6f2dc2a631e1d683cd02dc777404168
Beatriz <beatrizsallesss@gmail.com>
2021-01-01 19:26:28 -0300

    🔒 chore: Created security model.

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-// import Util from 'util';
-// import bcrypt from 'bcrypt';
+import Util from 'util';
+import bcrypt from 'bcrypt';
 
-// const hashAsync = Util.promisify(bcrypt.hash);
+const hashAsync = Util.promisify(bcrypt.hash);
 const schema = new mongoose.Schema({
   name: String,
   email: String,
@@ -10,17 +10,17 @@ const schema = new mongoose.Schema({
   role: String
 });
 
-// schema.pre('save', async function(next) {
-//   if (!this.password || !this.isModified('password')) {
-//     return next();
-//   }
-//   try {
-//     const hashedPassword = await hashAsync(this.password, 10);
-//     this.password = hashedPassword;
-//   } catch (error) {
-//     next(err);
-//   }
-// });
+schema.pre('save', async function(next) {
+  if (!this.password || !this.isModified('password')) {
+    return next();
+  }
+  try {
+    const hashedPassword = await hashAsync(this.password, 10);
+    this.password = hashedPassword;
+  } catch (error) {
+    next(err);
+  }
+});
 
 schema.set('toJSON', {
   transform: (doc, ret, options) => ({
